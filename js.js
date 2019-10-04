f5be544572f5acb6a5ef349d0601766bb6f1dfb1
Pei Li <leplep418@gmail.com>
2019-10-03 23:15:28 -0700

    update

--- js.js
+++ js.js
@@ -167,7 +167,6 @@
             }
         
             var sum = 0;
-            var ans = document.getElementById("input").value;
             if("+" == n2) {
                 sum = n1 + n3;
             } else if("-" == n2) {
